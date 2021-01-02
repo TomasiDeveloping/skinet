4ad6ab868905adec268caea41eeb999ffd5d14e6
Patrick Tomasi <info@tomasi-developing.ch>
2021-01-02 14:29:21 +0100

    ErrorHandling cleanUp

--- API/Helpers/ProductUrlResolver.cs
+++ API/Helpers/ProductUrlResolver.cs
@@ -2,20 +2,18 @@
 using AutoMapper;
 using Core.Entities;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace API.Helpers
 {
     public class ProductUrlResolver : IValueResolver<Product, ProductToReturnDto, string>
     {
         private readonly IConfiguration _config;
+
         public ProductUrlResolver(IConfiguration config)
         {
             _config = config;
         }
+
         public string Resolve(Product source, ProductToReturnDto destination, string destMember, ResolutionContext context)
         {
             if (!string.IsNullOrEmpty(source.PictureUrl))
